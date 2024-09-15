dd7473390810eccfa98e5e3f3f6e642bc427bd09
savelasquezo <savelasquezo@gmail.com>
2024-09-15 15:22:42 -0500

    Tizorbank v1.1

--- core/core/settings.py
+++ core/core/settings.py
@@ -92,7 +92,7 @@ INSTALLED_APPS = DJANGO_APPS + THIRD_APPS + CORE_APPS
 
 CRONPATH = os.path.join(BASE_DIR, 'logs', 'cron.log')
 CRONJOBS = [
-    #('0 0 * * *', 'apps.src.cron.AddFunds', f'>> {CRONPATH}'),
+    ('0 0 * * *', 'apps.src.cron.AddFunds', f'>> {CRONPATH}'),
 ]
 
 MIDDLEWARE = [
