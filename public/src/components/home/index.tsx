4d3b4cb45cc34e11294ac063adbf6c0192c95e4d
savelasquezo <savelasquezo@gmail.com>
2024-10-27 22:07:21 -0500

    MediaQuerys Home

--- public/src/components/home/index.tsx
+++ public/src/components/home/index.tsx
@@ -73,7 +73,7 @@ export default function Hero() {
               </ul>
             </div>
             <div className='flex flex-col lg:flex-col'>
-              <p className='w-full sm:w-[90%] lg:w-2/3 mt-4 lg:mt-10 font-cocogoose text-xs text-justify leading-none lg:leading-snug'>
+              <p className='w-full sm:w-[90%] lg:w-2/3 mt-4 lg:mt-10 font-cocogoose text-[0.65rem] lg:text-xs text-start lg:text-justify leading-none lg:leading-snug'>
                 Ahorrar de forma sencilla es posible con T-Bank. Abre tu cuenta y comienza a gestionar tus ahorros desde cualquier lugar. Haz crecer tu dinero dia a dia con nosotros.
                 <span className='ml-2 font-semibold cursor-pointer'>Mira Nuestras Tasas de Interes</span>
               </p>
@@ -84,26 +84,26 @@ export default function Hero() {
           </div>
         </div>
       </div>
-      <hr className="my-12 h-px border-t-0 bg-transparent bg-gradient-to-r from-transparent via-neutral-500 to-transparent opacity-25" />
-      <div className="w-full flex flex-col items-center justify-center mx-auto px-24 py-4">
-        <p className='font-cocogoose text-2xl font-semibold my-2 text-blue-950'>¿Como funciona T-Bank?</p>
-        <p className='w-1/2 mb-20 text-center font-cocogoose text-xs text-gray-800'>Ahorrar con nosotros es fácil y sin complicaciones. Abre tu cuenta y empieza a disfrutar de los intereses más altos del mercado, todo desde donde quieras. Si nos necesitas, siempre estaremos aquí para apoyarte.</p>
-        <ol className="w-full flex items-center justify-center">
-          <li className="w-1/3 flex flex-col items-center justify-center text-gray-200 gap-6 lg:px-12">
+      <hr className="h-px border-t-0 bg-transparent bg-gradient-to-r from-transparent via-neutral-500 to-transparent opacity-25 lg:my-12" />
+      <div className="w-full flex flex-col items-center justify-center mx-auto px-8 lg:px-24 py-4">
+        <p className='font-cocogoose text-2xl font-semibold my-2 text-blue-950 text-center'>¿Como funciona T-Bank?</p>
+        <p className='lg:w-1/2 mb-20 text-center font-cocogoose text-[0.65rem] lg:text-xs text-gray-800'>Ahorrar con nosotros es fácil y sin complicaciones. Abre tu cuenta y empieza a disfrutar de los intereses más altos del mercado, todo desde donde quieras. Si nos necesitas, siempre estaremos aquí para apoyarte.</p>
+        <ol className="w-full flex flex-col lg:flex-row items-center justify-center gap-12">
+          <li className="w-full lg:w-1/3 flex flex-col items-center justify-center text-gray-200 gap-6 lg:px-12">
             <Image priority width={360} height={360} src={"/assets/svg/login.svg"} className="h-32 w-auto ml-6 opacity-55" alt="" />
             <span className='flex flex-col items-center justify-center text-center'>
               <p className="leading-tight mb-2 font-cocogoose font-semibold uppercase text-gray-800">Incribirse</p>
               <p className="w-2/3 text-xs text-center font-cocogoose text-gray-600">Crea tu cuenta y completa la verificación de identidad de manera rápida y segura.</p>
             </span>
           </li>
-          <li className="w-1/3 flex flex-col items-center justify-center text-gray-200 gap-6">
+          <li className="w-full lg:w-1/3 flex flex-col items-center justify-center text-gray-200 gap-6 lg:px-12">
             <Image priority width={360} height={360} src={"/assets/svg/finance.svg"} className="h-32 w-auto opacity-55" alt="" />
             <span className='flex flex-col items-center justify-center text-center'>
               <p className="leading-tight mb-2 font-cocogoose font-semibold uppercase text-gray-800">Invertir</p>
               <p className="w-2/3 text-xs text-center font-cocogoose text-gray-600">Depósita fácilmente criptomonedas, mira nuestro instructivo si nunca lo has intentado</p>
             </span>
           </li>
-          <li className="w-1/3 flex flex-col items-center justify-center text-gray-200 gap-6">
+          <li className="w-full lg:w-1/3 flex flex-col items-center justify-center text-gray-200 gap-6 lg:px-12">
             <Image priority width={360} height={360} src={"/assets/svg/money.svg"} className="h-32 w-auto opacity-55" alt="" />
             <span className='flex flex-col items-center justify-center text-center'>
               <p className="leading-tight mb-2 font-cocogoose font-semibold uppercase text-gray-800">Ahorrar</p>
@@ -114,17 +114,17 @@ export default function Hero() {
       </div>
       <hr className="my-12 h-px border-t-0 bg-transparent bg-gradient-to-r from-transparent via-neutral-500 to-transparent opacity-25" />
       <div className='w-full h-full flex flex-col justify-start items-center my-12'>
-        <p className='text-xl font-cocogoose font-semibold'>¡Haz que tu dinero trabaje para ti de verdad!</p>
+        <p className='text-sm lg:text-xl font-cocogoose font-semibold'>¡Haz que tu dinero trabaje para ti de verdad!</p>
         <Video thumbnail={data?.thumbnail} url={data?.video} />
         <p className='w-11/12 lg:w-1/2 font-cocogoose text-xs lg:text-sm leading-tight text-center font-semibold'>Tendemos a ser cautelosos, especialmente en temas de dinero, pero también disfrutamos compartir lo que nos da seguridad y nos resulta beneficioso.​</p>
       </div>
       <hr className="my-12 h-px border-t-0 bg-transparent bg-gradient-to-r from-transparent via-neutral-500 to-transparent opacity-25" />
-      <div className='w-3/4 h-auto flex items-center justify-center mx-auto my-24'>
+      <div className='w-11/12 lg:w-3/4 h-auto flex items-center justify-center mx-auto lg:my-24'>
         <div className="flex flex-col rounded-md bg-gray-50 shadow-lg text-surface shadow-secondary-1 md:flex-row">
           <Image priority width={1440} height={880} src={"/assets/images/background07.jpg"} style={{ WebkitMaskImage: 'linear-gradient(to left, transparent 0%, black 20%)', maskImage: 'linear-gradient(to left, transparent 0%, black 20%)' }} className="h-64 w-auto rounded-t-lg object-cover rounded-s-md" alt="" />
           <div className="flex flex-col justify-start p-6">
             <p className="mb-2 text-xl font-cocogoose font-semibold">Lineas de Credito</p><br />
-            <p className="w-4/5 mb-4 text-xs font-cocogoose text-justify">
+            <p className="w-full lg:w-4/5 mb-4 text-xs font-cocogoose leading-none lg:leading-tight text-justify">
               Acceder a nuestras líneas de crédito es una excelente manera de hacer realidad tus metas, ya sea para proyectos personales o necesidades imprevistas.
               Con una tasa de interés de <span className='font-bold font-bankprinter text-lg'>1.4%</span> mensual, te ofrecemos una de las mejores opciones del mercado, diseñada para brindarte respaldo y confianza en cada paso.
             </p><br />
@@ -135,8 +135,8 @@ export default function Hero() {
         </div>
       </div>
       <Math />
-      <hr className="my-12 h-px border-t-0 bg-transparent bg-gradient-to-r from-transparent via-neutral-500 to-transparent opacity-25" />
-      <div className='relative w-full h-96 flex flex-row mt-12'>
+      <hr className="lg:my-12 h-px border-t-0 bg-transparent bg-gradient-to-r from-transparent via-neutral-500 to-transparent opacity-25" />
+      <div className='relative w-full h-60 lg:h-96 flex flex-row lg:mt-12'>
         <Image priority width={1800} height={1300} src={"/assets/images/background02.png"} className="hidden lg:block absolute top-0 right-0 w-auto h-96 object-contain" alt="" />
         <Image priority width={1800} height={1300} src={"/assets/images/background04.png"} className="block lg:hidden absolute top-0 right-0 w-auto h-96 object-contain" alt="" />
         <div className='w-full hidden lg:block lg:w-1/2'>-</div>
@@ -159,7 +159,7 @@ export default function Hero() {
                 ¡Haz que tu dinero trabaje para ti de verdad!
               </h5>
               <div className="mt-6 lg:mb-0 mb-6">
-              <div className='w-full flex flex-row items-center justify-center lg:justify-start gap-x-2.5 lg:gap-x-4 my-6 lg:my-0'>
+              <div className='w-full flex flex-row items-center justify-start gap-x-2.5 lg:gap-x-4 mb-20 lg:my-0'>
                 {data?.links?.map((link: any) => (
                   <a key={link.id} href={link.link || '#'} target="_blank" rel="noopener noreferrer" className={`w-auto h-10 flex items-center justify-center text-center opacity-90 hover:opacity-100 bg-gray-600 text-white px-3 py-2 font-xl rounded-full transition-opacity duration-300`}>
                     <span className='mt-0 flex items-center justify-center lg:block'>
@@ -176,10 +176,10 @@ export default function Hero() {
                   <span className="block uppercase text-gray-200 text-sm font-semibold mb-2">T-Bank</span>
                   <ul className="list-unstyled">
                     <li>
-                      <a className="text-gray-400 hover:text-gray-800 font-semibold block pb-2 text-sm" href="https://www.creative-tim.com/presentation?ref=njs-profile">¿Quienes Somos?</a>
+                      <a className="text-gray-400 hover:text-gray-200 font-semibold block pb-2 text-sm" href="https://www.creative-tim.com/presentation?ref=njs-profile">¿Quienes Somos?</a>
                     </li>
                     <li>
-                      <a className="text-gray-400 hover:text-gray-800 font-semibold block pb-2 text-sm" href="https://www.creative-tim.com/bootstrap-themes/free?ref=njs-profile">Soporte</a>
+                      <a className="text-gray-400 hover:text-gray-200 font-semibold block pb-2 text-sm" href="https://www.creative-tim.com/bootstrap-themes/free?ref=njs-profile">Soporte</a>
                     </li>
                   </ul>
                 </div>
@@ -187,13 +187,13 @@ export default function Hero() {
                   <span className="block uppercase text-gray-200 text-sm font-semibold mb-2">Servicios</span>
                   <ul className="list-unstyled">
                     <li>
-                      <a className="text-gray-400 hover:text-gray-800 font-semibold block pb-2 text-sm" href="https://blog.creative-tim.com?ref=njs-profile">Cuenta de Ahorros</a>
+                      <a className="text-gray-400 hover:text-gray-200 font-semibold block pb-2 text-sm" href="https://blog.creative-tim.com?ref=njs-profile">Cuenta de Ahorros</a>
                     </li>
                     <li>
-                      <a className="text-gray-400 hover:text-gray-800 font-semibold block pb-2 text-sm" href="https://www.github.com/creativetimofficial?ref=njs-profile">Inversiones</a>
+                      <a className="text-gray-400 hover:text-gray-200 font-semibold block pb-2 text-sm" href="https://www.github.com/creativetimofficial?ref=njs-profile">Inversiones</a>
                     </li>
                     <li>
-                      <a className="text-gray-400 hover:text-gray-800 font-semibold block pb-2 text-sm" href="https://www.creative-tim.com/bootstrap-themes/free?ref=njs-profile">Lineas de Credito</a>
+                      <a className="text-gray-400 hover:text-gray-200 font-semibold block pb-2 text-sm" href="https://www.creative-tim.com/bootstrap-themes/free?ref=njs-profile">Lineas de Credito</a>
                     </li>
                   </ul>
                 </div>
@@ -201,13 +201,13 @@ export default function Hero() {
                   <span className="block uppercase text-gray-200 text-sm font-semibold mb-2">Enlaces</span>
                   <ul className="list-unstyled">
                     <li>
-                      <a className="text-gray-400 hover:text-gray-800 font-semibold block pb-2 text-sm" href="https://creative-tim.com/terms?ref=njs-profile">Terminos & Condiciones</a>
+                      <a className="text-gray-400 hover:text-gray-200 font-semibold block pb-2 text-sm" href="https://creative-tim.com/terms?ref=njs-profile">Terminos & Condiciones</a>
                     </li>
                     <li>
-                      <a className="text-gray-400 hover:text-gray-800 font-semibold block pb-2 text-sm" href="https://creative-tim.com/privacy?ref=njs-profile">Aviso Legal</a>
+                      <a className="text-gray-400 hover:text-gray-200 font-semibold block pb-2 text-sm" href="https://creative-tim.com/privacy?ref=njs-profile">Aviso Legal</a>
                     </li>
                     <li>
-                      <a className="text-gray-400 hover:text-gray-800 font-semibold block pb-2 text-sm" href="https://creative-tim.com/contact-us?ref=njs-profile">Contactanos</a>
+                      <a className="text-gray-400 hover:text-gray-200 font-semibold block pb-2 text-sm" href="https://creative-tim.com/contact-us?ref=njs-profile">Contactanos</a>
                     </li>
                   </ul>
                 </div>
@@ -218,7 +218,7 @@ export default function Hero() {
           <div className="flex flex-wrap items-center md:justify-between justify-center">
             <div className="w-full md:w-4/12 px-4 mx-auto text-center">
               <div className="text-sm text-gray-200 font-semibold py-1">
-                Copyright © <span id="get-current-year">2024</span><span className="text-gray-200 hover:text-gray-800"/> Tizorbank
+                Copyright © <span id="get-current-year">2024</span><span className="text-gray-200 hover:text-gray-200"/> Tizorbank
               </div>
             </div>
           </div>
--- public/src/components/home/math/index.tsx
+++ public/src/components/home/math/index.tsx
@@ -1,15 +1,16 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useLanguage } from '@/utils/i18next';
 import Image from 'next/image';
-
+import { nextSite } from '@/utils/next-site';
 import { Input } from "@nextui-org/react";
 import { MdQrCode2 } from "react-icons/md";
 
 const InvestmentSimulator: React.FC = () => {
+  const [data, setData] = useState<{ [key: string]: any } | null>(null);
   const { t } = useLanguage();
 
-  const [amount, setAmount] = useState<number>(1000);
-  const [months, setMonths] = useState<number>(12);
+  const [amount, setAmount] = useState<number>(0);
+  const [months, setMonths] = useState<number>(3);
   const [interest, setInterest] = useState<number>(14);
 
   const handleKeyDown = (event: any) => {
@@ -18,82 +19,97 @@ const InvestmentSimulator: React.FC = () => {
     }
   };
 
-  const calculateCompoundInterest = (principal: number, annualRate: number, time: number) => {
-    const r = annualRate/1200;
-    return principal * Math.pow(1 + r, time);
+  const mathInterest = (months: number) => {
+    const minRate = data?.min_interest || 12;
+    const maxRate = data?.max_interest || 24;
+    return minRate + ((maxRate - minRate) * (months - 3))/(33);
   };
 
-  const total = calculateCompoundInterest(amount, interest, months);
+  const total = amount * Math.pow(1 + (interest / 1200), months);
+
+  useEffect(() => {
+    setInterest(mathInterest(months));
+  }, [months]);
+
+  useEffect(() => {
+    const fetchedSettings = nextSite();
+    if (fetchedSettings) {
+      setData(fetchedSettings);
+    }
+  }, []);
 
   return (
-    <section className='flex flex-row items-start justify-center px-32'>
-      <div className='w-1/2 flex flex-col items-start justify-start px-24 py-8 mx-auto mt-8'>
+    <section className='relative w-full flex flex-col lg:flex-row items-start justify-center px-4 lg:px-32'>
+      <div className='w-full lg:w-1/2 flex flex-col items-start justify-start px-4 lg:px-24 py-8 mx-auto mt-8 z-10'>
         <div className='flex flex-col items-start justify-center'>
-          <p className='font-cocogoose text-2xl font-semibold my-6 text-blue-950'>Simulador de Inversiones</p>
-          <p className='font-cocogoose text-xs text-gray-800 text-justify'>Ahorrar con nosotros es fácil y sin complicaciones. Abre tu cuenta y empieza a disfrutar de los intereses más altos del mercado, todo desde donde quieras. Si nos necesitas, siempre estaremos aquí para apoyarte.</p>
+          <p className='font-cocogoose text-center lg:text-start text-2xl font-semibold my-6 text-blue-950'>Simulador de Inversiones</p>
+          <p className='font-cocogoose text-[0.65rem] lg:text-xs text-gray-800 text-justify'>Ahorrar con nosotros es fácil y sin complicaciones. Abre tu cuenta y empieza a disfrutar de los intereses más altos del mercado, todo desde donde quieras. Si nos necesitas, siempre estaremos aquí para apoyarte.</p>
         </div>
-        <div className='flex flex-row items-start justify-center gap-x-12 py-8'>
-          <Input 
-            type="number" 
-            name='amount' 
-            variant={'underlined'} 
-            label="Inversión" 
-            onChange={(e) => setAmount(Number(e.target.value))} 
-            onKeyDown={handleKeyDown} 
-            min={100} 
-            max={100000} 
-            step={100} 
-          />
-          <Input 
-            type="number" 
-            name='months' 
-            variant={'underlined'} 
-            label="Temporalidad" 
-            onChange={(e) => setMonths(Number(e.target.value))} 
-            onKeyDown={handleKeyDown} 
-            min={3} 
-            max={36} 
-            step={3} 
-          />
-          <Input 
-            type="text" 
-            name='interest' 
-            variant={'underlined'} 
-            label="Interés (E.A)" 
-            value={`${interest}%`} 
-            onChange={(e) => setInterest(Number(e.target.value.replace('%', '')))} 
-            isDisabled
-            style={{ color: 'black', fontWeight: 'bold' }}
-            className="max-w-xs"
-            startContent={
-              <MdQrCode2 className="text-2xl text-default-400 pointer-events-none flex-shrink-0" />
-            }
-          />
-          <Input 
-            type="text" 
-            name='total' 
-            variant={'underlined'} 
-            label="$ Total" 
-            value={`$ ${total.toFixed(2)}`}
-            isDisabled
-            style={{ color: 'black', fontWeight: 'bold' }}
-            className="max-w-xs"
-            startContent={
-              <MdQrCode2 className="text-2xl text-default-400 pointer-events-none flex-shrink-0" />
-            }
-          />
+        <div className='w-full flex flex-col lg:flex-row items-start justify-center gap-8 py-8'>
+          <div className='w-full lg:w-1/2 flex flex-row gap-x-4'>
+            <Input
+              type="number"
+              name='amount'
+              variant={'underlined'}
+              label="Inversión"
+              onChange={(e) => setAmount(Number(e.target.value))}
+              onKeyDown={handleKeyDown}
+              min={100}
+              max={100000}
+              step={100}
+            />
+            <Input
+              type="number"
+              name='months'
+              variant={'underlined'}
+              label="Temporalidad"
+              onChange={(e) => setMonths(Number(e.target.value))}
+              onKeyDown={handleKeyDown}
+              min={3}
+              max={36}
+              step={3}
+            />
+          </div>
+          <div className='w-full lg:w-1/2 flex flex-row gap-x-4'>
+            <Input
+              type="text"
+              name='interest'
+              variant={'underlined'}
+              label="Interés (E.A)"
+              value={`${interest.toFixed(2)}%`}
+              isDisabled
+              style={{ color: 'black', fontWeight: 'bold' }}
+              className="max-w-xs"
+              startContent={
+                <MdQrCode2 className="text-2xl text-default-400 pointer-events-none flex-shrink-0" />
+              }
+            />
+            <Input
+              type="text"
+              name='total'
+              variant={'underlined'}
+              label="$ Total"
+              value={`$ ${total.toFixed(2)}`}
+              isDisabled
+              style={{ color: 'black', fontWeight: 'bold' }}
+              className="max-w-xs"
+              startContent={
+                <MdQrCode2 className="text-2xl text-default-400 pointer-events-none flex-shrink-0" />
+              }
+            />
+          </div>
         </div>
-        <div className='w-full flex items-center justify-center flex-row gap-x-6'>
-          <p className='w-3/4 font-cocogoose text-[0.55rem] text-gray-800 text-justify'>
+        <div className='w-full flex flex-col lg:flex-row items-center justify-center gap-6'>
+          <p className='w-full lg:w-3/4 font-cocogoose text-[0.55rem] text-gray-800 text-justify'>
             La simulación del CDT T-Bank te muestra un cálculo estimado del valor de tu inversión, el plazo y la tasa vigente al momento de la consulta. Recuerda que esta información es solo referencial y no constituye una oferta comercial. Informes
             <span className='font-semibold cursor-pointer ml-1'>Términos & Condiciones</span>
           </p>
-          <div className='w-1/4'>
+          <div className='w-full lg:w-1/4'>
             <button type="button" disabled className="flex justify-center items-center bg-gray-800 h-10 w-full text-white p-2 shadow-sm rounded-sm focus:bg-red-800 hover:bg-red-900 transition-colors duration-700">Solicitar</button>
           </div>
         </div>
       </div>
-      <div className='w-2/5 flex items-start justify-start -translate-x-16'>
+      <div className='absolute lg:static w-full lg:w-2/5 flex items-start justify-start lg:-translate-x-16 opacity-5 lg:opacity-100 grayscale lg:grayscale-0'>
         <Image priority width={1800} height={1300} src={"/assets/images/background05.png"} className="w-auto h-[30rem] object-contain" alt="" />
       </div>
     </section>
