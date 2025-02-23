b952be762c6ae2e257eeed9910955646746498ed
savelasquezo <savelasquezo@gmail.com>
2025-02-23 15:52:13 -0500

    Edit TBank for Tizor

--- public/src/app/layout.tsx
+++ public/src/app/layout.tsx
@@ -1,39 +1,38 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import { I18next } from '@/utils/i18next';
+import { I18next } from "@/utils/i18next";
 
-import Head from 'next/head';
-import Script from 'next/script';
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
-import '@/styles/styles.css';
+import "@/styles/styles.css";
 
 export const metadata: Metadata = {
   title: "Tizor",
   description: "Tizor",
 };
 
-export default function RootLayout({ children, }: Readonly<{ children: React.ReactNode; }>) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html>
-      <Head>
+    <html lang="en">
+      <head>
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
-        <Script strategy="afterInteractive" src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE}`}/>
-        <Script strategy="afterInteractive" id="google-analytics"
+      </head>
+      <body>
+        <Script strategy="afterInteractive" src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE}`} />
+        <Script
+          strategy="afterInteractive"
+          id="google-analytics"
           dangerouslySetInnerHTML={{
             __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', ${process.env.NEXT_PUBLIC_GOOGLE});
-          `,
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE}');
+            `,
           }}
         />
-      </Head>
-      <body>
-        <I18next>
-          {children}
-        </I18next>
+        <I18next>{children}</I18next>
       </body>
     </html>
   );
